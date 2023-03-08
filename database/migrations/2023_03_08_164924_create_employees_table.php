be9d29e37249f164060784d1cf83e333eb8733e4
Imammahdi170625 <imammahdizakaria75@gmail.com>
2023-03-08 23:00:44 +0600

    employee migration file finished

--- database/migrations/2023_03_08_164924_create_employees_table.php
+++ database/migrations/2023_03_08_164924_create_employees_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('password');
+            $table->string('photo');
             $table->timestamps();
         });
     }
